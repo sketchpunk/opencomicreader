ef1e26bc153d6eca77fd1df4fa0246c531149987
Constantin Wenger <constantin.wenger@googlemail.com>
2013-09-18 21:56:29 +0200

    Added options for the Size of the Listitems
    Conflicts:
    	res/values/strings.xml
    
    Resolved conflicts because of string externalizations

--- src/com/sketchpunk/ocomicreader/App.java
+++ src/com/sketchpunk/ocomicreader/App.java
@@ -1,10 +1,21 @@
 package com.sketchpunk.ocomicreader;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
 
 public class App extends Application{
 	/*========================================================
 	*/
 	@Override
 	public void onCreate(){ super.onCreate(); }//func
+	public static int convertDpToPixels(float dp, Context context){
+		Resources resources = context.getResources();
+		return (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP,
+				dp,
+				resources.getDisplayMetrics()
+				);
+	}
 }//cls

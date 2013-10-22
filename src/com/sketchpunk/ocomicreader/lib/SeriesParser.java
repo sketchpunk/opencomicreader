1d8a0f3440970cf9ed348efac126a456ba6e407e
VoR <vor@vor.com>
2013-10-22 08:50:36 -0400

    More intelligent parsing of the series name. Takes folder path instead of the file name, depending on which pattern is successful, the folder name will be considered the series name.

--- src/com/sketchpunk/ocomicreader/lib/SeriesParser.java
+++ src/com/sketchpunk/ocomicreader/lib/SeriesParser.java
@@ -9,10 +9,26 @@ public class SeriesParser{
 	
 	public SeriesParser(){
 		mParseList = new ArrayList<ParseItem>();
-		mParseList.add(new ParseItem("([\\s_]+v(ol)?(ume)?[\\s_]*\\d+)"));
-		mParseList.add(new ParseItem("([\\s_]+c(h)?(apter)?[\\s_]*\\d+)"));
-		mParseList.add(new ParseItem("([\\s_]+-)"));
-		mParseList.add(new ParseItem("([\\s_]+[tT]*\\d+)"));
+		
+		//Mangas tend to be grouped by parent folder, where the filename is just a variation of chapter or volume
+		mParseList.add(new ParseItem("/([\\s\\w\\.]+)/v(ol)?(ume)?[\\s_]*\\d+.*\\.(\\w{3})$")); // /[series]/[v|ol|ume] 000[garbage].cbr
+		mParseList.add(new ParseItem("/([\\s\\w\\.]+)/c(h)?(apter)?[\\s_]*\\d+.*\\.(\\w{3})$")); // /[series]/[c|h|apter] 000[garbage].cbr
+		
+		//Determine through file name with a variation of chapter or volume
+		mParseList.add(new ParseItem("/([\\s\\w]+)[\\s\\-_]*v(ol)?(ume)?[\\s_]*\\d+.*\\.(\\w{3})$")); // /[series] [-] [v|ol|ume] 000[garbage].cbr
+		mParseList.add(new ParseItem("/([\\s\\w]+)[\\s\\-_]*c(h)?(apter)?[\\s_]*\\d+.*\\.(\\w{3})$")); // /[series] [-] [c|h|apter] 000[garbage].cbr
+		
+		//Determine through filename with T following a number, Usually french books use t for TOME instead of chapter or volume.
+		mParseList.add(new ParseItem("/([\\s\\w]+)[\\s\\-_]*t(ome)?[\\s_]*\\d+.*\\.(\\w{3})$")); // /[series] [t|ome] 000[garbage].cbr
+		
+		//Determine through filename with a number near the end.
+		mParseList.add(new ParseItem("/([\\s\\w]+)[\\s\\-]+\\d+.*\\.(\\w{3})$")); // /[series] [-] 000[garbage].cbr
+
+		//Determine through filename with a - usually following a sub title with no number system.
+		mParseList.add(new ParseItem("/([\\s\\w]+)[\\s_]*\\-.*\\.(\\w{3})$")); // /[series] [-] [subtitle].cbr
+		
+		//When all else fails, Just get the filename without extension.
+		mParseList.add(new ParseItem("/(.*)\\.(\\w{3})$")); // /[series].cbr
 	}//func
 	
 	public String get(String txt){
@@ -20,6 +36,7 @@ public class SeriesParser{
 		String tmp;
 		for(int i=0; i < mParseList.size(); i++){
 			tmp = mParseList.get(i).parse(txt);
+			
 			if(!tmp.isEmpty()) return tmp;
 		}//for
 		return txt;
@@ -27,14 +44,17 @@ public class SeriesParser{
 	
 	protected static class ParseItem{
 		private Pattern mPat;
+		private int mSeriesGroup = 1; //Determine which group from the pattern is the series name.
 		
-		public ParseItem(String pat){
+		public ParseItem(String pat){ mPat = Pattern.compile(pat,Pattern.CASE_INSENSITIVE); }//func
+		public ParseItem(int seriesGrp,String pat){
 			mPat = Pattern.compile(pat);
+			mSeriesGroup = seriesGrp;
 		}//func
 		
 		public String parse(String txt){
 			Matcher m = mPat.matcher(txt);
-			if(m.find()) return txt.substring(0,m.start()).replace("_"," ").trim();
+			if(m.find()) return m.group(mSeriesGroup).replace("_"," ").trim(); //Remove any underscores and spaces from the end
 			return "";
 		}//func
 	}//cls
